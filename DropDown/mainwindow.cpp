ac2ae7ee81d1014bec08f0c803d3afab34aa770a
Nicholas Grigorian <4nicholasgrigorian@gmail.com>
2022-05-10 23:44:38 -0700

    Added DropDown folder

---
+++ DropDown/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "preferences.h"
+#include "./ui_mainwindow.h"
+#include "./ui_preferences.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionPreferences_triggered()
+{
+    Preferences *p = new Preferences();
+    p->show();
+}
+
